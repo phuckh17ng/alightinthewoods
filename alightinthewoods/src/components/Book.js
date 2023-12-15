80cc40430214726e7f7f6f4e64cdccc4bb651f74
Khang <khanglmp@nec.vn>
2023-12-15 18:06:32 +0700

    update

--- alightinthewoods/src/components/Book.js
+++ alightinthewoods/src/components/Book.js
@@ -1,7 +1,53 @@
 import React from "react";
-
-const Book = () => {
-  return <div>Book</div>;
+import styles from "./Book.module.scss";
+const Book = ({ imgUrl, title, index, bookMark }) => {
+  return (
+    <div className={styles.bookContainer}>
+      <img
+        className="min-h-screen h-full w-full object-cover aspect-auto"
+        src={imgUrl}
+        alt="chapter1"
+      />
+      <div className={styles.bookHoverMask}>
+        <div>
+          <div className={styles.leftBaton}></div>
+          <div className={styles.rightBaton}></div>
+          <img
+            className={styles.bookConner1}
+            src="https://alightinthewoods.net/images/borders/border_corner_piece.svg"
+            alt="conner"
+          />
+          <img
+            className={styles.bookConner2}
+            src="https://alightinthewoods.net/images/borders/border_corner_piece.svg"
+            alt="conner"
+          />
+          <img
+            className={styles.bookConner3}
+            src="https://alightinthewoods.net/images/borders/border_corner_piece.svg"
+            alt="conner"
+          />
+          <img
+            className={styles.bookConner4}
+            src="https://alightinthewoods.net/images/borders/border_corner_piece.svg"
+            alt="conner"
+          />
+        </div>
+      </div>
+      <div className={styles.bookMask}>
+        <img className={styles.bookMark} src={bookMark} alt="chapter1" />
+        <span className={styles.borderHorizonTop}></span>
+        <div className={styles.bookTitle}>
+          Hidden
+          <br /> Hollow
+        </div>
+        <div className={styles.bookNumber}>
+          <h1>0{index}</h1>
+          <p>BOOK</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Book;
--- alightinthewoods/src/components/Sidebar.js
+++ alightinthewoods/src/components/Sidebar.js
@@ -1,13 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Sidebar.module.scss";
 const Sidebar = () => {
+  const [sidebarShow, setSidebarShow] = useState(false);
+  const handleSidebarOnClick = () => {
+    setSidebarShow(!sidebarShow);
+  };
+
   return (
-    <div className={styles.container}>
-      <div className={styles.hamburger__icon}>
-        <div></div>
-        <div></div>
-        <div></div>
+    <div className="relative h-full">
+      <div className={styles.container}>
+        <div className={styles.hamburgerMenu}>
+          <div
+            className={styles.hamburger__icon}
+            onClick={handleSidebarOnClick}
+          >
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+        <div className={styles.sidebarNavigate}>
+          <div className={styles.sidebarChapter}>
+            <div className={styles.chapterIcon}>
+              <img
+                src={
+                  "https://alightinthewoods.net/images/sidebar-chapters-icon.svg"
+                }
+                alt="chapter"
+              />
+            </div>
+            <div className={styles.sidebarTitle}>BOOKS</div>
+          </div>
+          <div className={styles.sidebarChapter}>
+            <div className={styles.chapterIcon}>
+              <img
+                src={
+                  "https://alightinthewoods.net/images/sidebar-explore-icon.svg"
+                }
+                alt="chapter"
+              />
+            </div>
+            <div className={styles.sidebarTitle}>EXPLORE</div>
+          </div>
+          <div className="h-full flex flex-col items-center">
+            <span className={styles.borderHorizonTop}></span>
+            <div className={styles.sideBarMainTitle}>BOOKS</div>
+            <span className={styles.borderHorizonBottom}></span>
+          </div>
+        </div>
+        <div className={styles.sidebarSettings}>
+          <img
+            src={"https://alightinthewoods.net/images/settings-icon.svg"}
+            alt="settings"
+          />
+        </div>
       </div>
+
+      <div
+        className={`h-screen absolute top-0 left-[4.6rem] z-50 bg-[#222222] ${
+          sidebarShow ? "w-[600px]" : "w-0"
+        }`}
+      ></div>
     </div>
   );
 };
