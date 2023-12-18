4d16eb99c713a7eea6ea8e0f8015d47ed3747db2
phuckh17ng <lemauphuckhang171001@gmail.com>
2023-12-19 00:18:32 +0700

    update

--- alightinthewoods/src/components/Sidebar.js
+++ alightinthewoods/src/components/Sidebar.js
@@ -1,68 +1,74 @@
 import React, { useState } from "react";
 import styles from "./Sidebar.module.scss";
 const Sidebar = () => {
-  const [sidebarShow, setSidebarShow] = useState(false);
-  const handleSidebarOnClick = () => {
-    setSidebarShow(!sidebarShow);
-  };
+	const [sidebarShow, setSidebarShow] = useState(false);
+	const handleSidebarOnClick = () => {
+		setSidebarShow(!sidebarShow);
+	};
 
-  return (
-    <div className="relative h-full">
-      <div className={styles.container}>
-        <div className={styles.hamburgerMenu}>
-          <div
-            className={styles.hamburger__icon}
-            onClick={handleSidebarOnClick}
-          >
-            <div></div>
-            <div></div>
-            <div></div>
-          </div>
-        </div>
-        <div className={styles.sidebarNavigate}>
-          <div className={styles.sidebarChapter}>
-            <div className={styles.chapterIcon}>
-              <img
-                src={
-                  "https://alightinthewoods.net/images/sidebar-chapters-icon.svg"
-                }
-                alt="chapter"
-              />
-            </div>
-            <div className={styles.sidebarTitle}>BOOKS</div>
-          </div>
-          <div className={styles.sidebarChapter}>
-            <div className={styles.chapterIcon}>
-              <img
-                src={
-                  "https://alightinthewoods.net/images/sidebar-explore-icon.svg"
-                }
-                alt="chapter"
-              />
-            </div>
-            <div className={styles.sidebarTitle}>EXPLORE</div>
-          </div>
-          <div className="h-full flex flex-col items-center">
-            <span className={styles.borderHorizonTop}></span>
-            <div className={styles.sideBarMainTitle}>BOOKS</div>
-            <span className={styles.borderHorizonBottom}></span>
-          </div>
-        </div>
-        <div className={styles.sidebarSettings}>
-          <img
-            src={"https://alightinthewoods.net/images/settings-icon.svg"}
-            alt="settings"
-          />
-        </div>
-      </div>
+	return (
+		<div className="relative h-full">
+			<div className={styles.containerMask}></div>
+			<div className={styles.container}>
+				<div className={styles.hamburgerMenu}>
+					<div
+						className={styles.hamburger__icon}
+						onClick={handleSidebarOnClick}
+					>
+						<div></div>
+						<div></div>
+						<div></div>
+					</div>
+				</div>
+				<div className={styles.sidebarNavigate}>
+					<div className={styles.sidebarChapter}>
+						<div className={styles.chapterIcon}>
+							<img
+								src={
+									"https://alightinthewoods.net/images/sidebar-chapters-icon.svg"
+								}
+								alt="chapter"
+							/>
+						</div>
+						<div className={styles.sidebarTitle}>BOOKS</div>
+					</div>
+					<div className={styles.sidebarChapter}>
+						<div className={styles.chapterIcon}>
+							<img
+								src={
+									"https://alightinthewoods.net/images/sidebar-explore-icon.svg"
+								}
+								alt="chapter"
+							/>
+						</div>
+						<div className={styles.sidebarTitle}>EXPLORE</div>
+					</div>
+					<div className="h-full flex flex-col items-center">
+						<span className={styles.borderHorizonTop}></span>
+						<div className={styles.sideBarMainTitle}>BOOKS</div>
+						<span className={styles.borderHorizonBottom}></span>
+					</div>
+				</div>
+				<div className={styles.sidebarSettings}>
+					<img
+						src={"https://alightinthewoods.net/images/settings-icon.svg"}
+						alt="settings"
+					/>
+				</div>
+			</div>
 
-      <div
-        className={`h-screen absolute top-0 left-[4.6rem] z-50 bg-[#222222] ${
-          sidebarShow ? "w-[600px]" : "w-0"
-        }`}
-      ></div>
-    </div>
-  );
+			<div
+				className={`h-screen absolute top-0 left-[4.6rem] z-50 bg-[#222222] transition-all duration-700 ease-in-out ${
+					sidebarShow ? "w-[480px]" : "w-0"
+				}`}
+			></div>
+			<div
+				className={`${styles.sidebarBackdrop} ${
+					sidebarShow ? styles.show : styles.hide
+				}`}
+			></div>
+		</div>
+	);
 };
 
 export default Sidebar;
